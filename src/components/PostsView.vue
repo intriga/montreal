424d5de8453a97a883c15c40cd2863719e337720
intriga <intriga2402@gmail.com>
2024-04-06 10:12:41 -0400

    Module Posts

---
+++ src/components/PostsView.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="col-md-8 grid-sidebar" id="content">
+    <!-- start grid sidebar -->
+    <div
+      class="jelly_homepage_builder jl_nonav_margin homepage_builder_3grid_post jl_fontsize22 jl_cus_grid2 colstyle1"
+    >
+      <div class="jl_wrapper_row jl-post-block-416765">
+        <div class="row">
+          <div class="box blog_grid_post_style jl_row_1" v-for="value in posts" :key="value">
+            <div class="jl_grid_box_wrapper">
+              <div class="image-post-thumb">
+                <a
+                  href="#"
+                  class="link_image featured-thumbnail"
+                  title="Every photographer needs to shoot this photo"
+                >
+                  <img
+                    width="780"
+                    height="450"
+                    :src="value.image"
+                    class="attachment-disto_large_feature_image size-disto_large_feature_image wp-post-image"
+                    alt=""
+                  />
+                  <div class="background_over_image"></div>
+                </a>
+                <span class="meta-category-small"
+                  ><a class="post-category-color-text" style="background: #6b34ba" href="#"
+                    >Gaming</a
+                  ></span
+                >
+                <span class="jl_post_type_icon"><i class="la la-volume-up"></i></span>
+              </div>
+              <div class="post-entry-content">
+                <h3 class="image-post-title">
+                  <a :href="'/post/' + value.slug">{{ value.title }}</a>
+                </h3>
+                <span class="jl_post_meta">
+                  <span class="jl_author_img_w">
+                    <img
+                      src="../../public/img/favicon.jpg"
+                      width="30"
+                      height="30"
+                      class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo"
+                    />
+                    <a href="#" rel="author">Anna Nikova</a>
+                  </span>
+                  <span class="post-date"
+                    ><i class="fa fa-clock-o"></i>{{ format_date(value.created_at) }}</span
+                  >
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="clear_line_3col_home"></div>
+          <div class="jl-loadmore-btn-w">
+            <a href="#" class="jl_btn_load">Load more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end grid sidebar -->
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import moment from 'moment'
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  mounted() {
+    this.getPosts()
+  },
+
+  methods: {
+    getPosts() {
+      axios.get('http://127.0.0.1:8000/api/posts').then((res) => {
+        this.posts = res.data
+        console.log(this.posts)
+      })
+    },
+
+    format_date(value) {
+      if (value) {
+        return moment(String(value)).format('DD/MM/YYYY')
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/SliderView.vue
@@ -0,0 +1,263 @@
+<template>
+  <div class="col-md-12 jl_mid_main_3col">
+    <div class="jl_3col_wrapin">
+      <div id="pl-3375" class="panel-layout">
+        <div id="pg-3375-0" class="panel-grid panel-no-style">
+          <div id="pgc-3375-0-0" class="panel-grid-cell">
+            <div class="jelly_homepage_builder jl_car_home jl_nonav_margin">
+              <div class="jl_wrapper_row">
+                <div
+                  class="row jelly_loading_pro jelly_cus_h717 jl_fontsize22 jl_builder_4carousel car_style2 jl_hide_author jl_hide_author_img jl_hide_date jl_hide_arrow"
+                >
+                  <div class="col-md-3">
+                    <div class="jl_car_wrapper">
+                      <div class="jl_car_img_front">
+                        <span
+                          class="image_grid_header_absolute"
+                          style="
+                            background-image: url('http://jellywp.com/theme/disto/demo/wp-content/uploads/2019/02/bruce-christianson-559084-unsplash-1000x935.jpg');
+                          "
+                        ></span>
+                        <a
+                          href="#"
+                          class="link_grid_header_absolute"
+                          title="It’s always fun time and smile in the summer"
+                        ></a>
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #0015ff" href="#"
+                            >Business</a
+                          ></span
+                        >
+                      </div>
+                      <div class="post-entry-content">
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #0015ff" href="#"
+                            >Business</a
+                          ></span
+                        >
+                        <h3 class="image-post-title">
+                          <a href="#"> It’s always fun time and smile in the summer</a>
+                        </h3>
+                        <span class="jl_post_meta"
+                          ><span class="jl_author_img_w"
+                            ><img
+                              src="../public/js/favicon.jpg"
+                              width="30"
+                              height="30"
+                              alt="Anna Nikova"
+                              class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo"
+                            /><a href="#" title="Posts by Anna Nikova" rel="author"
+                              >Anna Nikova</a
+                            ></span
+                          ><span class="post-date"
+                            ><i class="fa fa-clock-o"></i>Dec 24, 2016</span
+                          ></span
+                        >
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="jl_car_wrapper">
+                      <div class="jl_car_img_front">
+                        <span
+                          class="image_grid_header_absolute"
+                          style="
+                            background-image: url('http://jellywp.com/theme/disto/demo/wp-content/uploads/2019/02/bradley-1320886-unsplash-1000x667.jpg');
+                          "
+                        ></span>
+                        <a
+                          href="#"
+                          class="link_grid_header_absolute"
+                          title="The great time for enjoy city view on mountain"
+                        ></a>
+                        <span class="jl_post_type_icon"><i class="la la-camera"></i></span>
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #d1783c" href="#"
+                            >Crazy</a
+                          ></span
+                        >
+                      </div>
+                      <div class="post-entry-content">
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #d1783c" href="#"
+                            >Crazy</a
+                          ></span
+                        >
+                        <h3 class="image-post-title">
+                          <a href="#"> The great time for enjoy city view on mountain</a>
+                        </h3>
+                        <span class="jl_post_meta"
+                          ><span class="jl_author_img_w"
+                            ><img
+                              src="../public/js/favicon.jpg"
+                              width="30"
+                              height="30"
+                              alt="Anna Nikova"
+                              class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo"
+                            /><a href="#" title="Posts by Anna Nikova" rel="author"
+                              >Anna Nikova</a
+                            ></span
+                          ><span class="post-date"
+                            ><i class="fa fa-clock-o"></i>Dec 24, 2016</span
+                          ></span
+                        >
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="jl_car_wrapper">
+                      <div class="jl_car_img_front">
+                        <span
+                          class="image_grid_header_absolute"
+                          style="
+                            background-image: url('http://jellywp.com/theme/disto/demo/wp-content/uploads/2018/11/pexels-photo-1036628-1000x667.jpeg');
+                          "
+                        ></span>
+                        <a
+                          href="#"
+                          class="link_grid_header_absolute"
+                          title="Outdoor photo shooting with sexy and beautiful"
+                        ></a>
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #ed1c1c" href="#"
+                            >Active</a
+                          ></span
+                        >
+                      </div>
+                      <div class="post-entry-content">
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #ed1c1c" href="#"
+                            >Active</a
+                          ></span
+                        >
+                        <h3 class="image-post-title">
+                          <a href="#"> Outdoor photo shooting with sexy and beautiful</a>
+                        </h3>
+                        <span class="jl_post_meta"
+                          ><span class="jl_author_img_w"
+                            ><img
+                              src="../public/js/favicon.jpg"
+                              width="30"
+                              height="30"
+                              alt="Anna Nikova"
+                              class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo"
+                            /><a href="#" title="Posts by Anna Nikova" rel="author"
+                              >Anna Nikova</a
+                            ></span
+                          ><span class="post-date"
+                            ><i class="fa fa-clock-o"></i>Dec 24, 2016</span
+                          ></span
+                        >
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="jl_car_wrapper">
+                      <div class="jl_car_img_front">
+                        <span
+                          class="image_grid_header_absolute"
+                          style="
+                            background-image: url('http://jellywp.com/theme/disto/demo/wp-content/uploads/2016/12/daniel-korpai-1435296-unsplash-1000x1250.jpg');
+                          "
+                        ></span>
+                        <a
+                          href="#"
+                          class="link_grid_header_absolute"
+                          title="10 best place for your travel around the world"
+                        ></a>
+                        <span class="jl_post_type_icon"><i class="la la-camera"></i></span>
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #36c942" href="#"
+                            >Sports</a
+                          ></span
+                        >
+                      </div>
+                      <div class="post-entry-content">
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #36c942" href="#"
+                            >Sports</a
+                          ></span
+                        >
+                        <h3 class="image-post-title">
+                          <a href="#"> 10 best place for your travel around the world</a>
+                        </h3>
+                        <span class="jl_post_meta"
+                          ><span class="jl_author_img_w"
+                            ><img
+                              src="../public/js/favicon.jpg"
+                              width="30"
+                              height="30"
+                              alt="Anna Nikova"
+                              class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo"
+                            /><a href="#" title="Posts by Anna Nikova" rel="author"
+                              >Anna Nikova</a
+                            ></span
+                          ><span class="post-date"
+                            ><i class="fa fa-clock-o"></i>Dec 24, 2016</span
+                          ></span
+                        >
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="jl_car_wrapper">
+                      <div class="jl_car_img_front">
+                        <span
+                          class="image_grid_header_absolute"
+                          style="
+                            background-image: url('http://jellywp.com/theme/disto/demo/wp-content/uploads/2016/12/daniel-korpai-1435296-unsplash-1000x1250.jpg');
+                          "
+                        ></span>
+                        <a
+                          href="#"
+                          class="link_grid_header_absolute"
+                          title="It feel like win when your code run no error"
+                        ></a>
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #6b34ba" href="#"
+                            >Gaming</a
+                          ></span
+                        >
+                      </div>
+                      <div class="post-entry-content">
+                        <span class="meta-category-small"
+                          ><a class="post-category-color-text" style="background: #6b34ba" href="#"
+                            >Gaming</a
+                          ></span
+                        >
+                        <h3 class="image-post-title">
+                          <a href="#"> It feel like win when your code run no error</a>
+                        </h3>
+                        <span class="jl_post_meta"
+                          ><span class="jl_author_img_w"
+                            ><img
+                              src="../public/js/favicon.jpg"
+                              width="30"
+                              height="30"
+                              alt="Anna Nikova"
+                              class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo"
+                            /><a href="#" title="Posts by Anna Nikova" rel="author"
+                              >Anna Nikova</a
+                            ></span
+                          ><span class="post-date"
+                            ><i class="fa fa-clock-o"></i>Dec 23, 2016</span
+                          ></span
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="ads300x250-thumb">
+              <a href="#">
+                <img src="../public/js/ads.jpg" alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
